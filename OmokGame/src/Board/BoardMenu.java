75ac8f0fdc4bc0876839afc7ab12c35ff818c3fd
carevilla <carevilla@miners.utep.edu>
2023-02-18 12:01:03 -0700

    Initial commit for assignment

---
+++ OmokGame/src/Board/BoardMenu.java
@@ -0,0 +1,18 @@
+package Board;
+
+public class BoardMenu {
+
+	public BoardMenu() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void drawBoard() {
+		
+	}
+	
+	public void updateBoard() {
+		
+	}
+	
+
+}
