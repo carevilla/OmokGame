4f76f3b73a31bdb1b5c621bf12de99a401b7316a
carevilla <carevilla@miners.utep.edu>
2023-02-22 20:47:19 -0700

    Added subclass AI of player

---
+++ OmokGame/src/Board/PlayerAi.java
@@ -0,0 +1,9 @@
+package Board;
+
+public class PlayerAi extends Player{
+
+	//protected super(String name , Boolean h , Boolean t , Boolean w){
+		//(String name , Boolean h , Boolean t , Boolean w);
+	//}
+
+}
